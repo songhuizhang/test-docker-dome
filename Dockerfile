caab4c4b72dcf35e03848ec230ef3c0265ff480b
songhuizhang <songhui25262@gmail.com>
2018-11-20 22:47:13 +0800

    初始化项目

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# 使用node版本 8.12.0
+FROM node:8.12.0
+
+RUN apt-get update \
+    && apt-get install -y nginx
+
+WORKDIR /app
+
+COPY . /app/
+
+EXPOSE 80
+
+RUN npm install \
+    && npm run build \
+    && cp -r dist/* /var/www/html \
+    && rm -rf /aa
+
+CMD ["nginx","-g","daemon off;"]
---
+++ build/precache-manifest.2006fc7afd497869271c5dd87c90ef22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f07a633fc377bb525b3b",
+    "url": "/static/js/main.f07a633f.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "f07a633fc377bb525b3b",
+    "url": "/static/css/main.5562c3b9.chunk.css"
+  },
+  {
+    "revision": "eecfa499fa5e63c3f015614840f7f8e0",
+    "url": "/index.html"
+  }
+];
